5e603ac87e01916b15c00f11f715c4a8220b0f13
heligie <heliemzh@gmail.com>
2023-08-07 23:30:10 +0300

    fix: change carousel

--- source/js/carousel.js
+++ source/js/carousel.js
@@ -1,16 +1,43 @@
-const previewButton = document.querySelector('.carousel__button--left');
-const nextButton = document.querySelector('.carousel__button--right')
-const quoteContainer = document.querySelectorAll('.carousel__quote')
+// Элементы карусели
 
-let active = 0;
+const carouselContainer = document.querySelector('.carousel__wrapper');
+const prevButton = document.querySelector('.carousel__button--previous');
+const nextButton = document.querySelector('.carousel__button--next');
+const quotes = Array.from(carouselContainer.querySelectorAll('.carousel__quote'));
+const quoteCount = quotes.length;
+let quoteIndex = 0;
 
-nextButton.onclick = () => {
-  quoteContainer[active].classList.remove('is-active');
-  if (active + 1 == quoteContainer.length) {
-    active = 0;
-  }
-  else {
-    active++;
-  }
-  quoteContainer[active].classList.add('is-active');
+// Обработчики событий для кнопок
+
+prevButton.addEventListener('click', showPreviousQuote);
+nextButton.addEventListener('click', showNextQuote);
+
+// Функция для показа предыдущего отзыва
+
+function showPreviousQuote() {
+  quoteIndex = (quoteIndex - 1 + quoteCount) % quoteCount;
+  updateCarousel();
 }
+
+// Функция для показа следующего отзыва
+
+function showNextQuote() {
+  quoteIndex = (quoteIndex + 1) % quoteCount;
+  updateCarousel();
+}
+
+// Функция для обновления отображения карусели
+
+function updateCarousel() {
+  quotes.forEach((quote, index) => {
+    if (index === quoteIndex) {
+      quote.style.display = 'flex';
+    } else {
+      quote.style.display = 'none';
+    }
+  });
+}
+
+// Инициализация карусели
+
+updateCarousel();
